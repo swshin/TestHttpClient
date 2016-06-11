df5c73f8e5812ad8fe5be8f59cb7876ffef9a312
swshin <swshin90@gmail.com>
2016-06-12 04:03:21 +0900

    organize imports

--- src/swshin/TestHttpClient.java
+++ src/swshin/TestHttpClient.java
@@ -3,7 +3,6 @@ package swshin;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.cert.Certificate;
